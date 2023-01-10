e241f14e980ad9a590e99ab504e6cd77b97284e0
Rohit <Rohit@spotblhol0128.awsspoton.com>
2023-01-10 19:51:24 +0530

    Added:Address book program.

---
+++ js/addressBookForm.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.name-error');
+    name.addEventListener('input', () => {
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if (nameRegex.test(name.value)) nameError.textContent = "";
+        else nameError.textContent = "Name is Incorrect";
+    })
+  
+    const phone = document.querySelector('#phone');
+    const phoneError = document.querySelector('.phone-error');
+    phone.addEventListener('input', function () {
+        let phoneRegex = RegExp('^[+]?[0-9]{2}\\s?[0-9]{10}$');
+        if (phoneRegex.test(phone.value)) phoneError.textContent = "";
+        else phoneError.textContent = "Phone Number is Incorrect";
+    })
+  
+    const address = document.querySelector('#address');
+    const addressError = document.querySelector('.address-error');
+    address.addEventListener('input', function () {
+        let addressRegex = RegExp('^[a-zA-Z0-9#,&\\s]{4,}$');
+        if (addressRegex.test(address.value)) addressError.textContent = "";
+        else addressError.textContent = "Address is Incorrect";
+    })
+  });
+  
+ 
